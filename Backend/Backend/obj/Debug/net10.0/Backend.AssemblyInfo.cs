9155b96570d9361aa9d897495cb20e97058ff51a
josue-balbontin <balbontinjosue@gmail.com>
2026-03-31 14:58:01 -0400

    actualizando bd

--- Backend/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77ccb7e6d1052fe8ac176f3b261efe46f5f46ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b27684f4fc0106adabbd52157217828121131e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
