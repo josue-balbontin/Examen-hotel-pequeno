c35b4e35130b8cf17a4f5e9727a6fbf7a88922d9
josue-balbontin <balbontinjosue@gmail.com>
2026-05-16 01:11:39 -0400

    volviendo atras

--- Backend/Backend/Modelos/Entidades/HotelDbContext.cs
+++ Backend/Backend/Modelos/Entidades/HotelDbContext.cs
@@ -32,8 +32,12 @@ public partial class HotelDbContext : DbContext
     public virtual DbSet<UsuariosReserva> UsuariosReservas { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=hotel_pequeno;Username=postgres;Password=273153");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=hotel_pequeno;Username=postgres;Password=273153");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
