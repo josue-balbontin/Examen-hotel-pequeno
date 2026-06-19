a4e8987a62be8a0a8da7f045f5954a2352819412
josue-balbontin <balbontinjosue@gmail.com>
2026-06-19 07:44:59 -0400

    HU2 agregar test para Dado que existen huespedes y habitaciones cuando complete los datos entonces el sistema debe registrarlo correctamente

--- Backend/Testback/CrearReservaHabitacion.cs
+++ Backend/Testback/CrearReservaHabitacion.cs
@@ -11,6 +11,40 @@ namespace Testback;
 // HU2 
 public class CrearReservaHabitacion
 {
- 
+    private Mock<IReservaRepositorio> _reservaRepoMock;
+    private Mock<IConfiguracionRepositorio> _configRepoMock;
+    private ReservaServicio _reservaServicio;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _reservaRepoMock = new Mock<IReservaRepositorio>();
+        _configRepoMock = new Mock<IConfiguracionRepositorio>();
+        _reservaServicio = new ReservaServicio(_reservaRepoMock.Object, _configRepoMock.Object);
+    }
+    
+    [Test]
+    public void RegistrarReserva_CamposCompletos_RegistroCorrecto()
+    {
+        /*Dado que existen huéspedes y habitaciones precargadas, cuando el usuario
+        complete los datos requeridos de la reserva, entonces el sistema debe
+        registrarla correctamente.*/
+        
+        var dto = new CrearReservaDto {
+            IdsUsuarios = new List<int> { 1, 2 },
+            IdHabitacion = 10,
+            FechaIngreso = new DateOnly(2023, 10, 1),
+            FechaSalida = new DateOnly(2023, 10, 5)
+        };
+
+        _reservaRepoMock.Setup(r => r.ObtenerCapacidadHabitacion(10)).Returns(2);
+        _reservaRepoMock.Setup(r => r.ExisteSolapamiento(10, dto.FechaIngreso, dto.FechaSalida)).Returns(false);
+
+        _reservaServicio.CrearReserva(dto);
+
+        _reservaRepoMock.Verify(r => r.Crear(It.IsAny<Reserva>(), dto.IdsUsuarios), Times.Once);
+    }
+    
+    
     
 }
